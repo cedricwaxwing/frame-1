8357733deedc3e00e125a0ed0b1292950276973d
Colin Spence <udappia@gmail.com>
2024-02-07 20:04:11 +0100

    adds query params

--- app/App.jsx
+++ app/App.jsx
@@ -1,31 +1,46 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Box, OrbitControls } from '@react-three/drei';
 import './App.css';
-import ScreenshotTrigger from './ScreenshotTrigger'; // Adjust the import path as needed
+import ScreenshotTrigger from './ScreenshotTrigger';
 
 const App = () => {
   const [triggerScreenshot, setTriggerScreenshot] = useState(false);
-  const color = 'red';
+  const [bgColor, setBgColor] = useState('ff0000');
+  const [fgColor, setFgColor] = useState('0000ff');
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search);
+
+    const bg = queryParams.get('bg');
+    if (bg) {
+      setBgColor(`#${bg}`);
+    }
+
+    const fg = queryParams.get('fg');
+    if (fg) {
+      setFgColor(`#${fg}`);
+    }
+  }, []);
 
   return (
     <>
       <Canvas
         style={{ width: '100vw', height: '75vh' }}
+        camera={{ position: [5, 5, 5] }}
         gl={{
           preserveDrawingBuffer: true,
           antialias: true,
         }}
-        camera={{ position: [5, 5, 5] }}
       >
         <ScreenshotTrigger trigger={triggerScreenshot} setTrigger={setTriggerScreenshot} />
         <directionalLight position={[10, 20, -20]} intensity={2} />
         <ambientLight intensity={1} />
-        <color attach="background" args={[color ? color : 'blue']} />
+        <color attach="background" args={[bgColor]} />
         <Box scale={4}>
-          <meshStandardMaterial color="blue" />
+          <meshStandardMaterial color={fgColor} />
         </Box>
         <OrbitControls />
       </Canvas>
--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -4,7 +4,7 @@ import { NEXT_PUBLIC_URL } from '../../config';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress: string | undefined = '';
-  let text: string | undefined = '';
+  const colorsDefault: string = '0000ff,00ff00';
 
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
@@ -13,22 +13,19 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     accountAddress = message.interactor.verified_accounts[0];
   }
 
-  if (message?.input) {
-    text = message.input;
-  }
+  const colors = message?.input ? message.input.split(',') : colorsDefault.split(',');
 
   if (message?.button === 3) {
-    return NextResponse.redirect(
-      'https://www.google.com/search?q=cute+dog+pictures&tbm=isch&source=lnms',
-      { status: 302 },
-    );
+    return NextResponse.redirect(`${NEXT_PUBLIC_URL}??bg=${colors[0]}&fg=${colors[1]}`, {
+      status: 302,
+    });
   }
 
   return new NextResponse(
     getFrameHtmlResponse({
       buttons: [
         {
-          label: `BLAHHHHHH: ${text}`,
+          label: `BLAHHHHHH: ${colors[0]}`,
         },
       ],
       image: `${NEXT_PUBLIC_URL}/park-2.png`,
